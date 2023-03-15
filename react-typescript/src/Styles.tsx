fefa697a802f9e3fcb60f94ae3b4ceb6222ede19
NabajyotiModak <naba93981@gmail.com>
2023-03-15 13:32:25 +0530

    styles prop

---
+++ react-typescript/src/Styles.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./components/Container"
+
+export default function Styles() {
+    return (
+        <div>
+            <Container styles={{ border: '1px solid black', padding: '1rem', display: "inline-block" }} />
+        </div>
+    )
+}
