597177c61a9e76daa9eb44ca5558ba5238e5f58d
NabajyotiModak <naba93981@gmail.com>
2023-03-19 12:40:06 +0530

    React.ComponentType<> for component as prop

---
+++ react-hook-typescript/src/components/auth/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+  return (
+    <div>
+      <p>Logged In</p>
+    </div>
+  )
+}
---
+++ react-hook-typescript/src/components/auth/Private.tsx
@@ -0,0 +1,16 @@
+import Login from "./Login"
+import { ProfileProps } from "./Profile"
+
+type PrivateProps = {
+    isLoggedIn: boolean,
+    Component: React.ComponentType<ProfileProps>
+}
+
+export const Private = ({ isLoggedIn, Component }: PrivateProps) => {
+    if (isLoggedIn) {
+        return <Component name="Nabajyoti Modak aka Papai" />
+    }
+    else {
+        return <Login />
+    }
+}
