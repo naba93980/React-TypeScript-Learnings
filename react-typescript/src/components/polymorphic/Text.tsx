912aa4e563d358b15903f6ade41e4be17c46d449
NabajyotiModak <naba93981@gmail.com>
2023-03-20 22:09:27 +0530

    polymorphic components

---
+++ react-typescript/src/components/polymorphic/Text.tsx
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+type TextOwnProps<C extends React.ElementType> = {
+    as?: C
+    size?: 'sm' | 'md' | 'lg',
+    color?: 'primary' | 'secondary'
+    children: React.ReactNode
+}
+
+type TextProps<C extends React.ElementType> = TextOwnProps<C> & Omit<React.ComponentProps<C>, keyof TextOwnProps<C>>  // Omit removes the common props from the React.ComponentProps<C>
+
+export const Text = <C extends React.ElementType>({ as, size, color, children }: TextProps<C>) => {
+    let Component = as || 'div'
+    return <Component className={`class-with-${size}-${color}`}>{children}</Component>
+} 
